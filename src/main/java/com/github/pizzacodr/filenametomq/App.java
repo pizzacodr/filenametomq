f350195c153c16d954d9e5fd48f6cef109551c22
pizzacodr <pizzacodr@users.noreply.github.com>
2019-06-29 18:45:00 -0400

    Initial work with WatchService

--- src/main/java/com/github/pizzacodr/filenametomq/App.java
+++ src/main/java/com/github/pizzacodr/filenametomq/App.java
@@ -1,13 +1,35 @@
 package com.github.pizzacodr.filenametomq;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException, InterruptedException
     {
-        System.out.println( "Hello World!" );
+        //constantly watch a directory
+    	WatchService watchService = FileSystems.getDefault().newWatchService();
+    	Path path = Paths.get("/home/yuri/watchDir");
+    	WatchKey watchKey = path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
+    	
+    	
+    	while ((watchKey = watchService.take()) != null) {
+            for (WatchEvent<?> event : watchKey.pollEvents()) {
+                System.out.println(
+                  "Event kind:" + event.kind() 
+                    + ". File affected: " + event.context() + ".");
+            }
+            watchKey.reset();
+        }
+    	//read in any files names that are placed in there
+    	
+    	//insert each filename into a message queue
+    	
     }
 }
